d88b25951b1c9266c74f50ef7666a546d23666b5
en <ehsiaoz@h2cap.com>
2016-11-21 00:38:40 -0600

    modal

--- assets/javascript/plumber.js
+++ assets/javascript/plumber.js
@@ -5,14 +5,60 @@ var locationsGeo = [];
 
 //Functions========================================================================================
 
+$(window).load(function(){
+        $('#home').modal('show');
+    });
+
 $(document).ready(function() {
 
 //Buttons===========================
-		$('#search-button').on('click', function onclick() {
-			$('#search-results').empty();
-			locationsGeo = [];
-			yelpSearch();
-	});
+
+//Search Button
+$('#page-search-button').on('click', function onclick() {
+	$('#search-results').empty();
+	locationsGeo = [];
+	yelpSearch();
+});
+
+//Autoselection value in location input field on click
+ $('#page-location-input').click( function highlight() {
+    $(this).select();
+  });
+
+
+//Modal================================
+// Get the modal
+var modal = document.getElementById('searchModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("homeModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+
+
+
+
+
 
 });
 
@@ -117,7 +163,7 @@ function yelpSearch () {
 		};
 
 	var terms = 'plumber';
-	var near = $('#location-input').val().trim();
+	var near = $('#page-location-input').val().trim();
 
 	var accessor = {
 			consumerSecret : auth.consumerSecret,
